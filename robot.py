9f8aaee81f068eda0a7910ca5e336d93019678b5
Niroo Arjuna <niroo.arjuna@gmail.com>
2019-06-28 13:07:27 +1000

    Added robot.py script

---
+++ robot.py
@@ -0,0 +1,118 @@
+# Code by Niroo Arjuna
+
+class Robot:
+    orientation = ["EAST", "WEST", "NORTH", "SOUTH"]
+
+    def __init__(self):
+        #Initially declare the variables of the Toy Robot
+        self.x_position = None
+        self.y_position = None
+        self.width = 5
+        self.height = 5
+        self.facing = None
+        self.placed = False
+
+    def place(self, x, y, facing):
+        #Initially checking to ensure that coordinates are in the 5x5 Square Top
+        if self.coordinates_in_boundry(x, y) is False:
+            return
+
+        #Check if the orientation is valid
+        if facing not in self.orientation:
+            print("Toy robot not placed in unknown orientation!")
+            return
+
+        #Robot placed if rob inside the square top and orientation is valid
+        self.placed = True
+        self.x_position = x
+        self.y_position = y
+        self.facing = facing
+        print("Toy robot has been successfully placed!")
+
+    def move(self):
+        if self.placed is False:
+            print("Toy robot has not been placed")
+
+        if self.facing == "EAST":
+            x, y = self.x_position + 1, self.y_position
+        elif self.facing == "WEST":
+            x, y = self.x_position - 1, self.y_position
+        elif self.facing == "NORTH":
+            x, y = self.x_position, self.y_position + 1
+        elif self.facing == "SOUTH":
+            x, y = self.x_position, self.y_position - 1
+        else:
+            print("Toy Robot placed in unknown oritentaion and cannot be moved")
+
+        if self.coordinates_in_boundry(x,y) is True:
+            #Move the Toy Robot
+            self.x_position = x
+            self.y_position = y
+            print("Toy Robot has been successfully moved")
+
+    def left(self):
+        if self.placed is False:
+            print("Toy robot has not been placed")
+            return
+
+        turn_left = \
+            [
+                # current facing, left of current facing
+                ("EAST", "NORTH"),
+                ("WEST", "SOUTH"),
+                ("SOUTH", "EAST"),
+                ("NORTH", "WEST"),
+            ]
+
+        #Facing, Left
+        for (f, l) in turn_left:
+            if self.facing == f:
+                self.facing = l
+                print("Toy robot turned left")
+                break
+
+    def right(self):
+        if self.placed is False:
+            print("Toy robot has not been placed")
+            return
+
+        turn_right = \
+            [
+                # current facing, left of current facing
+                ("EAST", "SOUTH"),
+                ("WEST", "NORTH"),
+                ("SOUTH", "WEST"),
+                ("NORTH", "EAST"),
+            ]
+
+        # Facing, Right
+        for (f, r) in turn_right:
+            if self.facing == f:
+                self.facing = r
+                print("Toy robot turned right")
+                break
+
+    def report(self):
+        if self.placed is False:
+            print("Toy robot has not been placed")
+            return
+
+
+        info = "({0}, {1}, {2})".format(self.x_position, self.y_position, self.facing)
+        print(info)
+        return info
+
+    def coordinates_in_boundry(self, x, y):
+        if x < 0 or x >= self.width: #Maximum self.width is 5
+            print("X coordinate is not valid!, please enter a value between 0 & 5")
+            return False
+
+        if y < 0 or y >= self.height: #Maximum self.height is 5
+            print("Y coordinate is not valid!, please enter a value between 0 & 5")
+            return False
+
+        return True
+
+
+
+
